3f778cd437bb8ab45f2b56d87c7bddbab1960770
Texac56 <texax.vaic@gmail.com>
2024-09-18 15:40:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+//        int i = 0;
+//        while(i <= 10) {
+//            System.out.println("element: " + i);
+//            i++;
+        int i = 100;
+        do {
+            System.out.println("element: "+ i);
+            i *= 100;
+        }while(i < 10);
+        }
+    }
